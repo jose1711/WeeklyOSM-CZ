d01e75b88ba888775904824ece937903b6a91c58
tkas <tomas.kasparek@gmail.com>
2016-06-12 08:57:37 +0200

    initial header,footer and scripts

---
+++ bin/weekly.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+IN=`pwd`/$2
+
+test "$1" -gt 300 || { echo 'issue number should be bigger then 300!'; exit; }
+test -f "$IN"  || { echo 'no input file with issue text found!'; exit; }
+
+cd ~/documents/osm/weekly/ || exit
+
+cat header.html $IN footer.html >$1.html
+
+cp $i.html $1-orig.html
+
+sedfile 's/>About us</>O nás</;
+         s/>Mapping</>Mapování</;
+         s/>Community</>Komunita</;
+         s/>Imports</>Importy</;
+         s/>OpenStreetMap Foundation</>Nadace OpenStreetMap</;
+         s/>Humanitarian OSM</>Humanitární OSM</;
+         s/>Maps</>Mapy</;
+         s/>switch2OSM</>Přechod k OSM</;
+         s/>Open Data</>Otevřená data</;
+         s/>Programming</>Programování</;
+         s/>Releases</>Nová vydání</;
+         s/>Did you know .</>Vědeli jste ...</;
+         s/>Other .geo. things</>Ostatní "geo" záležitosti</;
+         s/>Upcoming Events</>Plánované události</;
+         s/>Long term dates</>Pozdější důležité akce</;
+         
+         s/>Where</>kde</;
+         s/>What</>co</;
+         s/>When</>kdy</;
+         s/>Country</>země</;
+         
+         s/>‘’’/>/g;
+
+         s/provided by the /poskytuje /;
+        ' $1.html
+
+vim $1.html
